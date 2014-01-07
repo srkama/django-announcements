bd4de70d347454170fbc3c53a12fc61af463cad0
srkama <sri.pgp@gmail.com>
2014-01-07 16:35:41 +0530

    added comments

--- announcements/context_processors.py
+++ announcements/context_processors.py
@@ -1,11 +1,20 @@
-from models import Announcement
+'''
+context processor for announcements
+'''
+__author__ = 'kamal.s'
+
+from announcements.models import Announcement
 import datetime
 
 
 def announcements(request):
-    announcement_objects = Announcement.objects.filter(publish_start__lte=datetime.datetime.now(),
-                                                       publish_end__gte=datetime.datetime.now())
-
+    """
+    adds valid announcements to the context dict
+    """
+    announcement_objects = Announcement.objects.filter(
+        publish_start__lte=datetime.datetime.now(),
+        publish_end__gte=datetime.datetime.now())
     if not request.user.is_authenticated():
-        announcement_objects = announcement_objects.filter(members_only__exact=False)
+        announcement_objects = announcement_objects.filter(
+            members_only__exact=False)
     return {'announcements': announcement_objects}
